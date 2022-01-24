04bbea594e5cc519d1aaf66dd603f69a51a271fc
najitestech <42984888+najitestech@users.noreply.github.com>
2022-01-24 15:00:49 -0500

    Create ip-check.sh

---
+++ ip-check.sh
@@ -0,0 +1,9 @@
+while read -r ip
+    do
+        if [[ $ip == "$1" ]]; then
+            shift
+            printf '%s\n' 'This IP is in the List'
+        else
+            printf '%s\n' 'IP Not Found'
+        fi
+    done < ./
